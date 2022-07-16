0ce6c005eac8f6df4bdc93f5e7131e7ca2093e38
Antonin <antonin.risser@gmail.com>
2022-07-16 16:52:08 +0200

    Retourne la liste des utilisateurs à purger

--- index.js
+++ index.js
@@ -9,18 +9,36 @@ const client = new Discord.Client({
 });
 
 client.on('ready', () => {
-    client.channels.fetch('802229980867199026').then((channel) => {
-        channel.messages.fetch('996336354348777552').then((message) => {
-            message.reactions.cache.forEach(async(reaction) => {
-                // Liste des utilisateurs ayant répondus avec l'icone '✋'
-                if (reaction._emoji.name === '✋') {
-                    reaction.users.fetch().then(function (users) {
-                        console.log(users);
+    client.guilds.fetch('777327735348527116').then((guilds)=> {
+        guilds.members.fetch().then((members) => {
+            // Contient la liste de tout les utilisateurs du serveur.
+            const allUsers = [];
+            const allUsersResponded = [];
+            members.filter(member => member.user.bot === false).forEach((member) => {
+                allUsers.push(member.user);
+            })
+            // On fetch le channel
+            client.channels.fetch('802229980867199026').then((channel) => {
+                // On fetch le message
+                channel.messages.fetch('996336354348777552').then((message) => {
+                    message.reactions.cache.forEach(async(reaction) => {
+                        // Liste des utilisateurs ayant répondus avec l'icone '✋'
+                        if (reaction._emoji.name === '✋') {
+                            reaction.users.fetch().then(function (user) {
+                                // La liste de tout les utilisateur ayant répondu.
+                                user.forEach((member) => {
+                                    allUsersResponded.push(member);
+                                })
+                                const utilisateurPurger = allUsers.filter(f => !allUsersResponded.includes(f));
+                                console.log('Utilisateur à purger');
+                                console.log(utilisateurPurger);
+                            });
+                        }
                     });
-                }
-            });
-        })
-    })
+                })
+            })
+        });
+    });
 });
 
 client.login(config.token);
