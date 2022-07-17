8e464c9003c8141a9eba607581d0472832b10271
Antonin <antonin.risser@gmail.com>
2022-07-17 02:56:40 +0200

    Update ready.js

--- src/event/ready.js
+++ src/event/ready.js
@@ -1,15 +1,7 @@
-const exa  = require('../command/example');
-const clearCMD  = require('../command/clear-cmd');
-const sendDM  = require('../command/send-dm');
 
 module.exports = async (client) => {
     client.logger.info(`[!] ${client.user.username} is now started...`)
-    client.logger.info(`[!] The bot have ${client.commandes.size} commands and ${client.slash.size} (/) commands`)
+    client.logger.info(`[!] The bot have ${client.slash.size} (/) commands`)
     client.user.setActivity(` être développé par nous.`, {type: 'PLAYING'})
 
-    let commands = client.application?.commands;
-
-    commands.create(exa);
-    commands.create(clearCMD);
-    commands.create(sendDM);
 };
