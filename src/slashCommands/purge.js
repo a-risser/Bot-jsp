add41074beceabfed299f0a7043ebc7016a97f15
Antonin <antonin.risser@gmail.com>
2022-07-18 23:09:56 +0200

    Commande de PURGE.

---
+++ src/slashCommands/purge.js
@@ -0,0 +1,46 @@
+module.exports = {
+    name: 'purge',
+    description: 'Commande envoyant un DM pour la purge.',
+    usage: '<prefix>purge [userIdentifiant] [expulsion]',
+    examples: ['purge userIdentifiant:id expulsion:true'],
+    options: [
+        {
+            name: 'user-identifiant',
+            description: "Définir le user à qui envoyer le MP",
+            type: 3,
+            required: true
+        },
+        {
+            name: 'expulsion',
+            description: "S'il faut expulser l'utilisateur ou non",
+            type: 5,
+            required: true
+        },
+    ],
+
+    run :async (client, interaction) => {
+        // On récupère le serveur
+        client.guilds.fetch('777327735348527116').then((guilds)=> {
+            // On vérifie que l'utilisateur est admin
+            guilds.members.fetch(interaction.member.user.id).then((guildMember) => {
+                if(guildMember._roles.includes('777328480584859658')) {
+                    guilds.members.fetch(interaction.options.getString('user-identifiant')).then((memberToExclude) => {
+                        const expulsion = interaction.options.getBoolean('expulsion');
+                        if (expulsion) {
+                            memberToExclude.send("Salut, tu vas être exclu !");
+
+                            memberToExclude.kick();
+                        } else {
+                            memberToExclude.send(
+                                "Salut,\n" +
+                                "La purge sur ᒍᔕᑭ-ᴶᵉᵘ ˢᵃᶦˢ ᵖᵃˢ- est terminée, et tu y as survécu grâce à ta réaction au message l'annonçant.\n" +
+                                "Cependant, on continue de vouloir avoir des membres actifs, et si tu reçois ce message c'est que tu ne l'es pas.\n" +
+                                "Ce message est donc là pour te prévenir que si tu n'es pas actif·ve d'ici la prochaine purge, la réaction à l'annonce ne te sauvera pas."
+                            );
+                        }
+                    });
+                }
+            });
+        });
+    }
+}
