ecee6c6297403cb86537644825cd67b622de61cf
Tek <tekpike@outlook.com>
2022-08-01 03:21:01 +0200

    improve GetGameEmoji script

--- src/utils/emoji.js
+++ src/utils/emoji.js
@@ -1,5 +1,5 @@
 function GetGameEmoji(game) {
-    let emojiName = game.name.replace(/[^a-zA-Z0-9]/g, '').trim()
+    let emojiName = game.name.replace(/[^a-zA-Z0-9\s]/g, '').trim()
         .split(/ |\B(?=[A-Z])/)
         .map(word => word.toLowerCase())
         .join('_')
