f4e01b09cb7ef545e14dfb1e3bbc25687bac10b9
Tek <tekpike@outlook.com>
2022-08-02 22:00:46 +0200

    Quelques tests sur les calls

--- src/event/messageCreate.call.js
+++ src/event/messageCreate.call.js
@@ -1,13 +1,11 @@
 const {GetGameEmoji} = require("../utils/emoji");
-const {MessageActionRow, MessageButton } = require("discord.js");
 
 module.exports = async (client, message) => {
     if (message.channel.id !== client.config.channelCmdAdminId) {return;} //todo: client.config.channelQuiJoueId
     if (message.author.bot) {return;}
     if (!message.mentions?.roles) {return;}
 
-    const messageUser = message.author;
-    const guild = message.guild;
+    const user = message.author;
 
     //Get games roles
     const games = message.mentions.roles.filter(role => /[^\u0000-\u00ff]/.test(role.name)); //keep roles with emoji only
@@ -20,16 +18,32 @@ module.exports = async (client, message) => {
         const emoji = GetGameEmoji(game);
         //add emoji to message
         message.react(emoji).catch((error) => {
-            client.logger.error('command: call, user:' + messageUser.username + ', reason: ' + error);
+            client.logger.error('command: call, user:' + user.username + ', reason: ' + error);
         });
     })
 
+    //Create reply thread
+    message.startThread({
+        name: 'Discutez du call dans ce fil',
+        autoArchiveDuration: 60,
+    }).catch((error) => {
+        client.logger.error('command: call, user:' + user.username + ', reason: ' + error);
+    });
+
+    //Create new thread
+   /* message.channel.threads.create({
+        name: 'Discutez du call dans ce fil',
+        autoArchiveDuration: 60,
+    }).catch((error) => {
+        client.logger.error('command: call, user:' + user.username + ', reason: ' + error);
+    });*/
+
     //add hashtag emoji
-    const hashtagEmoji = guild.emojis.cache.find(emoji => emoji.name === 'create_thread');
+    /*const hashtagEmoji = guild.emojis.cache.find(emoji => emoji.name === 'create_thread');
     await message.react(hashtagEmoji);
 
     const filter = (reaction, user) => {
-        return [hashtagEmoji.name].includes(reaction.emoji.name) && user.id === messageUser.id;
+        return [hashtagEmoji.name].includes(reaction.emoji.name);
     };
 
     message.awaitReactions({filter, max: 1})
@@ -40,30 +54,14 @@ module.exports = async (client, message) => {
             reaction.remove();
 
             message.startThread({
-                name: 'Discutez du call de ' + messageUser.username + ' dans ce fil',
+                name: 'Discutez du call de ' + user.username + ' dans ce fil',
                 autoArchiveDuration: 60,
             })
                 .catch((error) => {
-                    client.logger.error('command: call, user:' + messageUser.username + ', reason: ' + error);
+                    client.logger.error('command: call, user:' + user.username + ', reason: ' + error);
                 });
-        });
+        });*/
 
-    //Create new thread
-    /*message.channel.threads.create({
-        name: 'Discutez du call de ' + user.username + ' dans ce fil',
-        autoArchiveDuration: 60,
-    }).catch((error) => {
-        client.logger.error('command: call, user:' + user.username + ', reason: ' + error);
-    });*/
-
-    //Create reply thread
-    /*message.startThread({
-        name: 'Merci d\'en discuter dans ce fil',
-        autoArchiveDuration: 60,
-        reason: 'Discuter à propos du call.'
-    }).catch((error) => {
-        client.logger.error('command: call, user:' + user.username + ', reason: ' + error);
-    });*/
 
     //Button to create thread
     /*const button = new MessageButton()
